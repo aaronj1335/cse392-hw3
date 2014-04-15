83b5241df99a979e0e36390e697cd25315e77f07
Aaron Stacy <aaron.r.stacy@gmail.com>
2014-04-15 10:13:58 -0500

    i dunno random cleanup

--- src/euler.cpp
+++ src/euler.cpp
@@ -1,5 +1,4 @@
 #include <omp.h>
-#include <stdio.h>
 
 #include "euler.h"
 
@@ -10,7 +9,8 @@ void eulerTour(midlvl_t const* const mids, const size_t l, size_t *const inIdxs,
     for (size_t i = 0; i < l; i++) {
       lvl_t lv = lvl(mids[i]);
       size_t inIdx = i * 2 - (size_t) lv;
-      size_t outIdx = inIdx + 2 * subtreeSize(mids, l, i);
+      size_t subtree = subtreeSize(mids, l, i);
+      size_t outIdx = inIdx + 2 * subtree + 1;
       inIdxs[i] = inIdx;
       outIdxs[i] = outIdx;
     }
